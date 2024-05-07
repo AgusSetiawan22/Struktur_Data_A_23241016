391313e3ec6b7b168c3115c801cd40eac8395131
AgusSetiawan22 <145946933+AgusSetiawan22@users.noreply.github.com>
2024-05-07 12:41:57 +0800

    Add files via upload

---
+++ meert4.py
@@ -0,0 +1,42 @@
+# inputan
+# aretmatika + - x :
+
+
+# coba = input("masuakn nama :")
+# print("coba")
+
+# bilangan_random = input("masukan bilangan acak")
+# print("bilangan_random")
+
+# ARETMATIAKA
+
+X = 9
+y = 3
+
+# PENJUMLAHAN
+hasil = X + y
+print(X, "+", y, '=' hasil)
+
+# PERKALIAN
+hasil = X * y
+print(X, "*", y, '=' hasil)
+hasil = X + y
+print(X, "+" ,y, '=' hasil)
+
+
+
+# PEMBAGIAN
+hasil = X / y
+print(X, "/" ,y, '=' hasil)
+
+# floor division//
+hasil = X // y
+print(X, 'Floor', '=', hasil)
+
+# SKALA PRIORITAS (), **, */:, +/-
+X = 3
+Y = 4
+Z = 5
+hasil = ( X, '**', y * (Z + X) / y - z % X // y)
+print(hasil)
+print( X, '**', y, '*', Z, '+', X, '/', y, '-', Z, %,)
