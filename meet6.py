830469c4e0d1e4cec299a607e6329a63979617df
AgusSetiawan22 <145946933+AgusSetiawan22@users.noreply.github.com>
2024-05-14 12:10:27 +0800

    Add files via upload
    Pertemuan ke enam (6)

---
+++ meet6.py
@@ -0,0 +1,60 @@
+# perbandingan >, <=, <= ==, !=, is, isnot,
+
+x = 3
+y = 4
+
+# lebih besar >
+hasil = y > x
+print (x, '>', y, '=', hasil)
+
+# lebih kecil <
+hasil = y > x
+print (x, '<', y, '=', hasil)
+
+# lebih besar >=
+hasil = y >= x
+print (x, '<=', y, '=', hasil)
+print (x, '<=', 4, '=', hasil)
+
+# lebih kecil <=
+hasil = y <= x
+print (x, '<=', y, '=,', hasil)
+print (x,'<=', 3, '=', hasil)
+
+
+
+
+a = 5
+b = 10
+c = 15
+
+# sama dengan ==
+print("Apakah a sama dengan b?")
+print(a, '==', b, '=', a == b)
+
+print("Apakah b sama dengan c?")
+print(b, '==', c, '=', b == c)
+
+print("Apakah a sama dengan 5?")
+print(a, '==', 5, '=', a == 5)
+
+print("Apakah c sama dengan 15?")
+print(c, '==', 15, '=', c == 15)
+
+
+a = 5
+b = 8
+c = 10
+
+# tidak sama dengan !=
+print("Apakah a tidak sama dengan b?")
+print(a, '!=', b, '=', a != b)
+
+print("Apakah b tidak sama dengan c?")
+print(b, '!=', c, '=', b != c)
+
+print("Apakah a tidak sama dengan 8?")
+print(a, '!=', 8, '=', a != 8)
+
+print("Apakah c tidak sama dengan 5?")
+print(c, '!=', 5, '=', c != 5)
