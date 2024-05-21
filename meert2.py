48680274ee5609265563f9ae40beed485c16ac38
AgusSetiawan22 <145946933+AgusSetiawan22@users.noreply.github.com>
2024-05-21 11:46:54 +0800

    Add files via upload
    Pertemuan ke Dua

---
+++ meert2.py
@@ -0,0 +1,13 @@
+# hari ini kita belajar variabel
+# Assigment  value
+
+x = 3 # assigment pertama
+y = 9
+print ("Nilai UAS sy adalah" , y)
+print ("Nilai UTS sy adalah" , x)
+
+jumlah_a = 100 # tidak boelh spasi
+bungkus10 = 5000 # tidak boleh diawal angka
+
+x = 1000 # assigment kedua 
+print ("Nilai UAS sy adalah" , x)
