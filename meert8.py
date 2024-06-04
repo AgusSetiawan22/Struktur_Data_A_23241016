35c9799985a3ad25310820af34b30d7c305b4656
AgusSetiawan22 <145946933+AgusSetiawan22@users.noreply.github.com>
2024-06-04 11:39:44 +0800

    Add files via upload

---
+++ meert8.py
@@ -0,0 +1,21 @@
+# belajar percabangan 
+
+nama = input("masukan nama :")
+
+# if kondisi : aksi
+#if nama=="edy" : print("dosen ganteng")
+#print ("program selsai")
+ 
+# if kondisi : aksi (idnetitas)
+if nama=="edy": 
+    print("dosen ganteng")
+    print("dosen ganteng bangat")
+print ("program selsai")
+
+
+if nama=="edy": 
+    print("dosen ganteng")
+    print(f"dosen ganteng bangat, makasih {nama}")
+else: 
+     print("ah ngak ganteng")
+print (f"program selsai, thnks {nama} ")
