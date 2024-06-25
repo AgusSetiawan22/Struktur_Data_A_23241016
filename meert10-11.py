4c34c4e3d55fdc6e29ecac80e413e81d7c6303e1
AgusSetiawan22 <145946933+AgusSetiawan22@users.noreply.github.com>
2024-06-25 11:57:56 +0800

    Add files via upload

---
+++ meert10-11.py
@@ -0,0 +1,34 @@
+# belajar perulangan (loop)
+
+#bil = 1
+#print(bil)
+#bil = bil + 2
+#print(bil)
+#bil = bil + 2
+#print(bil)
+#bil = bil + 2
+#print(bil)
+
+#for kondisi
+#   aksi
+
+#pengguna loop pada pemrograman lain
+#for (int i; i <=10, i++) {
+#   aksi
+#   aksi
+# }
+
+#peluang menggunakan list
+bil = [0,6,8,12,19,]
+print(bil)
+for i in bil :
+    print(f"nilai i sekarang -> (i)")
+print("ahkir program i")
+
+#for bil in range
+bil2 = range(5)
+for i in bil2 :
+    #print(f"nilai i sekarang -> (i)
+    print("pak edy dosen ganteng")
+
+print("akhir program 2")
